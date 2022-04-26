f84d10e447ad0c90ecd425c118c234ac1bc8e5c2
windcolor-dev <teaglehao@gmail.com>
2022-04-26 16:23:58 -0700

    Cleanup

--- src/main/java/ga/windpvp/statistics/StatisticsConnection.java
+++ src/main/java/ga/windpvp/statistics/StatisticsConnection.java
@@ -15,11 +15,57 @@ public class StatisticsConnection {
 	 */
 	private static Executor connectionPool = Executors.newCachedThreadPool();
 
+	/**
+	 * The time out for the keep alive in seconds
+	 */
 	public volatile int keepAliveTimeOutTime = 180;
+	
+	/**
+	 * The server's players
+	 */
 	public volatile int players = 0;
 	
+	/**
+	 * Whether this connection has been unregistered
+	 */
 	private volatile boolean hasDeregistered = false;
 	
+	/**
+	 * Starts the keep alive handler
+	 */
+	private void startKeepAliveHandler() {
+		// Handle keepalives
+		Runnable keepAliveRunnable = (() -> {
+			
+			while (true) {
+
+				// Check for keepalive expiry every second
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				// See if the keepalive is expired
+				if (keepAliveTimeOutTime == 0 && !hasDeregistered) {
+					closeConnection();
+				} else if (hasDeregistered) {
+					break;
+				}
+
+				// Decrement client's keepalive count down
+				keepAliveTimeOutTime--;
+
+			}
+		});
+
+		// Start keep alive thread
+		connectionPool.execute(keepAliveRunnable);
+	}
+	
+	/**
+	 * Closes the connection
+	 */
 	public void closeConnection() {
 		// Deregister
 		Statistics.connectionList.remove(this);
@@ -29,51 +75,28 @@ public class StatisticsConnection {
 		hasDeregistered = true;
 	}
 
+	/**
+	 * Starts the connection
+	 * @param clientSocket The client's socket
+	 */
 	public void startConnection(Socket clientSocket) {
 		// Initializes a new connection from a client
 		Runnable connectionRunnable = (() -> {
-			BufferedReader in;
 			try {
 				// Log to console
 				Logger.log("New connection established.");
 				// The connected client's input
-				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-				String inputLine;
-
-				// Handle keepalives
-				Runnable keepAliveRunnable = (() -> {
-					
-					while (true) {
-
-						// Check for keepalive expiry every second
-						try {
-							TimeUnit.SECONDS.sleep(1);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-
-						// See if the keepalive is expired
-						if (keepAliveTimeOutTime == 0 && !hasDeregistered) {
-							closeConnection();
-						} else if (hasDeregistered) {
-							break;
-						}
-
-						// Decrement client's keepalive count down
-						keepAliveTimeOutTime--;
-
-					}
-				});
-
-				// Start keep alive thread
-				connectionPool.execute(keepAliveRunnable);
-
+				// Handle keep alives
+				startKeepAliveHandler();
+				
 				boolean newServerLock = false;
 				boolean removedServerLock = false;
 
 				// Handle client messages
-				while ((inputLine = in.readLine()) != null) {
+				String clientInput;
+				while ((clientInput = in.readLine()) != null) {
 
 					// Exit connection if keep alive has expired
 					if (hasDeregistered) {
@@ -81,18 +104,18 @@ public class StatisticsConnection {
 					}
 
 					// Exit connection if told to do so by the client
-					if (inputLine.equalsIgnoreCase(".")) {
+					if (clientInput.equalsIgnoreCase(".")) {
 						break;
 
 						// Register a new server
-					} else if (inputLine.equalsIgnoreCase("new server")) {
+					} else if (clientInput.equalsIgnoreCase("new server")) {
 						if (!newServerLock) {
 							newServerLock = true;
 							Statistics.servers.incrementAndGet();
 						}
 
 						// Remove a server
-					} else if (inputLine.equalsIgnoreCase("removed server")) {
+					} else if (clientInput.equalsIgnoreCase("removed server")) {
 						if (!removedServerLock && newServerLock && !hasDeregistered) {
 							
 							// Prevent the client from removing the server multiple times
@@ -107,12 +130,12 @@ public class StatisticsConnection {
 						}
 
 						// Update keepalive status
-					} else if (inputLine.equalsIgnoreCase("keep alive packet")) {
+					} else if (clientInput.equalsIgnoreCase("keep alive packet")) {
 						keepAliveTimeOutTime = 100;
-					} else if (inputLine.toLowerCase().contains("player count packet ")) {
+					} else if (clientInput.toLowerCase().contains("player count packet ")) {
 						try {
 							// Get the player count in the string
-							players = Integer.valueOf(inputLine.toLowerCase().replace("player count packet ", ""));
+							players = Integer.valueOf(clientInput.toLowerCase().replace("player count packet ", ""));
 
 						} catch (NumberFormatException e) {
 							continue;
